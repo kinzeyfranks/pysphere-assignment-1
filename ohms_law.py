c0d20f6e0d52edd530749a26ae9f28fca10bde89
Kinzeyfranks <Kinzeyfranks1@gmail.com>
2025-08-13 00:15:31 +0100

    Added solution for Ohm's Law assignment

---
+++ ohms_law.py
@@ -0,0 +1,7 @@
+current = float(input("Enter the current in amperes (A): "))
+
+resistance = float(input("Enter the resistance in ohms (Ω): "))
+
+voltage = current * resistance
+
+print("The voltage is", voltage, "volts (V)")
